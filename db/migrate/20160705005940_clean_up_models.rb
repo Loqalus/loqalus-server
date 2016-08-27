1f0c0f0a6f7879d9b7cac190b2760357a78401cb
Jmbrace <jmbrace@ilstu.edu>
2016-08-27 12:02:20 -0500

    fixed typo

--- db/migrate/20160705005940_clean_up_models.rb
+++ db/migrate/20160705005940_clean_up_models.rb
@@ -1,10 +1,10 @@
 class CleanUpModels < ActiveRecord::Migration
   def change
-  	add_column :events, :action_type, :integer
-  	add_column :campaigns, :action_type, :integer
-  	add_column :conversations, :action_type, :integer
-   	remove_column :events, :type, :integer
-  	remove_column :campaigns, :type, :integer
-  	remove_column :conversations, :type, :integer
+  	# add_column :events, :action_type, :integer
+  	# add_column :campaigns, :action_type, :integer
+  	# add_column :conversations, :action_type, :integer
+   # 	remove_column :events, :type, :integer
+  	# remove_column :campaigns, :type, :integer
+  	# remove_column :conversations, :type, :integer
   end
 end
