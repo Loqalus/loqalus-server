595a9a8aa5e6b003b729eab56f8878c569b3ae13
Jmbrace <jmbrace@ilstu.edu>
2016-10-30 21:34:02 -0500

    updates

--- app/controllers/api/pins_controller.rb
+++ app/controllers/api/pins_controller.rb
@@ -9,8 +9,8 @@ class Api::PinsController < Api::BaseController
     long = pins_params[:long].to_f()
     dist = params[:dist].to_i()
     filter = false
-    if params.has_key?(:uid) 
-      uid = params[:uid]
+    if ! (request.headers[:HTTP_USER_ID] == 'null')
+      uid = request.headers[:HTTP_USER_ID]
       @user = User.find(uid)
       filter = true
       user_tags = @user.tag_list
@@ -23,6 +23,9 @@ class Api::PinsController < Api::BaseController
     if filter
       pins = filter_pins(pins, user_tags)
     end
+    if pins.length >= 10
+        pins = pins[0..9]
+    end
 
     render  :json => {:pins => pins}
   end
@@ -33,12 +36,11 @@ class Api::PinsController < Api::BaseController
     def filter_pins(pins, user_tags)
       pin_rankings = Array.new(pins.length, 0)
       pin_counter = 0
-      dist_factor = 0
 
       for pin in pins
         dist_factor = dist_factorie(pin_counter)
         tags_on_pin = pin.tag_list
-        pin_rankings[pin_counter] = pin_rankings[pin_counter] + dist_factor
+        pin_rankings[pin_counter] = pin_rankings[pin_counter] + (dist_factor)
 
         for tag in user_tags
 
@@ -50,19 +52,21 @@ class Api::PinsController < Api::BaseController
 
         pin_counter+=1
       end
-      puts pin_rankings
-      # ONLY RETURN TOP TEN
       return sort_pins(pins, pin_rankings)
     end
 
-    def dist_factorie(dist_rank)
-      return 0
+    def dist_factorie(pin_counter)
+      if pin_counter == 0
+        return 5
+      end
+
+      return (4/pin_counter)
     end
 
     def sort_pins(pins, pin_rankings)
       pairs = pins.zip(pin_rankings)
-      pairs.sort! {|a, b| a[1] <=> b[1]}
-      pairs.map(&:first)
+      pairs.sort! {|a, b| b[1] <=> a[1]}
+      pairs = pairs.map(&:first)
       return pairs 
     end
 
